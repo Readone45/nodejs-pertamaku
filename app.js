d0ade9c356b7b94da3cc9d5bd9d2c9dbd297a1d8
Ade Ridwan <sukun024@gmail.com>
2019-05-28 21:53:15 +0700

    Menambahkan baris untuk menggunakan express di node

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-//menampilkan pesan di console
-console.log("HelloWorld");
+//inisialisasi var / const untuk menampung lib express
+const express = require('express');
+const app = express();
+
+//menampilkan halaman web dengan express
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    res.write("ini adalah menu home");
+    res.end();
+});
+
+//menghubunkan ke port 8000
+app.listen(8000, () => {
+    console.log("layanan berjalan di port 8000");
+});
